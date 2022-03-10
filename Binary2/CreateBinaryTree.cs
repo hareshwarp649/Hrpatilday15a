d8963495d4e0cf50239e6bde9c00ae34ea008f0c
Harry <hareshwarp649>
2022-03-10 14:06:33 +0530

    Added the element using size method Binary tree

---
+++ Binary2/CreateBinaryTree.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree1
+{
+    internal class CreateBinaryTree<T> where T : IComparable
+    {
+        public T nodeData { get; set; }
+        public CreateBinaryTree<T> LeftTree { get; set; }
+        public CreateBinaryTree<T> RightTree { get; set; }
+        public CreateBinaryTree(T Data)
+        {
+            this.nodeData = Data;
+            this.LeftTree = null;
+            this.RightTree = null;
+        }
+        int leftCount = 0;
+        int rightCount = 0;
+        private bool result;
+        public void Insert(T Item)
+        {
+            T CurrNodeVal = this.nodeData;
+            if ((CurrNodeVal.CompareTo(Item)) > 0)
+            {
+                if (this.LeftTree == null)
+                {
+                    this.LeftTree = new CreateBinaryTree<T>(Item);
+                }
+                else
+                {
+                    this.LeftTree.Insert(Item);
+                }
+            }
+            else
+            {
+                if (this.RightTree == null)
+                {
+                    this.RightTree = new CreateBinaryTree<T>(Item);
+                }
+                else
+                {
+                    this.RightTree.Insert(Item);
+                }
+            }
+        }
+        public void Display()
+        {
+            if (this.LeftTree != null)
+            {
+                this.leftCount++;
+                this.LeftTree.Display();
+            }
+            Console.WriteLine(this.nodeData.ToString());
+
+            if (this.RightTree != null)
+            {
+                this.rightCount++;
+                this.RightTree.Display();
+            }
+        }
+    }
+}
---
+++ Binary2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to the Binary Search Tree");
+            CreateBinaryTree<int> atom= new CreateBinaryTree<int>(56);
+
+            Console.WriteLine("Enter 1.to value insertion\n  Enter 2.to insert  Element\n");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            switch (num)
+            {
+                case 1:
+                    atom.Insert(30);
+                    atom.Insert(70);
+                    atom.Display();
+                    break;
+
+                case 2:
+                    atom.Insert(30);
+                    atom.Insert(70);
+                    atom.Insert(35);
+                    atom.Insert(56);
+                    atom.Insert(20);
+                    atom.Insert(38);
+                    atom.Insert(45);
+                    atom.Insert(65);
+                    atom.Insert(15);
+                    atom.Insert(18);
+                    atom.Insert(25);
+                    atom.Insert(22);
+                    atom.Insert(92);
+                    atom.Insert(84);
+                    atom.Insert(78);
+                    atom.Display();
+                    
+                    break;
+
+                default:
+                    Console.WriteLine("Enter valid option!!!");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
