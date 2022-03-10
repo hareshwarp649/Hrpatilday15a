6f015a65ced7b74d81b3511ec609cc4ffee32bc8
Harry <hareshwarp649>
2022-03-10 13:48:04 +0530

    Create BST by adding value

---
+++ Binary1/CreateBst.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySeaWo
+{
+    public class CreateBst<T> where T : IComparable<T>
+    
+    {
+        public T nodeData { get; set; }
+        public CreateBst<T> leftTree { get; set; }
+
+        public CreateBst<T> rightTree { get; set; }
+
+        public CreateBst(T data)
+        {
+            this.nodeData = data;
+            this.leftTree = null;
+            this.rightTree = null;
+        }
+        int leftCount = 0, rightCount = 0;
+       
+        public void Insert(T item)
+        {
+            T CurrNodeVal = this.nodeData;
+            if ((CurrNodeVal.CompareTo(item)) > 0)
+            {
+                if (this.leftTree == null)
+                {
+                    this.leftTree = new CreateBst<T>(item);
+                }
+                else
+                {
+                    this.leftTree.Insert(item);
+                }
+            }
+            else
+            {
+                if (this.rightTree == null)
+                {
+                    this.rightTree = new CreateBst<T>(item);
+                }
+                else
+                {
+                    this.rightTree.Insert(item);
+
+                }
+            }
+        }
+
+        public void Display()
+        {
+            if (this.leftTree != null)
+            {
+                this.leftCount++;
+                this.leftTree.Display();
+            }
+            Console.WriteLine(this.nodeData.ToString());
+            if (this.rightTree != null)
+            {
+                this.rightCount++;
+                this.rightTree.Display();
+            }
+        }
+
+    }
+}
---
+++ Binary1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySeaWo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Binary Search Tree");
+            CreateBst<int> tree = new CreateBst<int>(56);
+
+            Console.WriteLine("Enter 1.to value insertion\n");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            switch (num)
+            {
+                case 1:
+                    tree.Insert(30);
+                    tree.Insert(70);
+                    tree.Display();
+                    break;
+
+                default:
+                    Console.WriteLine("Enter valid option!!!");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
